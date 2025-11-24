eb461d76d453529746aef1781c564759ae1d8a22
Andre Ferreira <andrerhrf@gmail.com>
2025-11-24 18:38:02 -0300

    fixes

--- sdks/javascript/rollup.config.js
+++ sdks/javascript/rollup.config.js
@@ -3,70 +3,72 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
+const plugins = [
+  nodeResolve({
+    preferBuiltins: true,
+  }),
+  commonjs(),
+  babel({
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: '16',
+          },
+        },
+      ],
+    ],
+  }),
+];
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
       file: 'dist/index.js',
       format: 'cjs',
       sourcemap: true,
+      inlineDynamicImports: true,
     },
-    {
+    plugins,
+    external: ['ws'],
+  },
+  {
+    input: 'src/index.js',
+    output: {
       file: 'dist/index.esm.js',
       format: 'esm',
       sourcemap: true,
+      inlineDynamicImports: true,
     },
-    {
+    plugins,
+    external: ['ws'],
+  },
+  {
+    input: 'src/index.js',
+    output: {
       file: 'dist/index.umd.js',
       format: 'umd',
       name: 'VectorizerClient',
       sourcemap: true,
+      inlineDynamicImports: true,
     },
-    {
+    plugins,
+    external: ['ws'],
+  },
+  {
+    input: 'src/index.js',
+    output: {
       file: 'dist/index.umd.min.js',
       format: 'umd',
       name: 'VectorizerClient',
       sourcemap: true,
-      plugins: [terser()],
+      inlineDynamicImports: true,
     },
-  ],
-  plugins: [
-    nodeResolve({
-      preferBuiltins: true,
-    }),
-    commonjs(),
-    babel({
-      babelHelpers: 'bundled',
-      exclude: 'node_modules/**',
-      presets: [
-        [
-          '@babel/preset-env',
-          {
-            targets: {
-              node: '16',
-            },
-          },
-        ],
-      ],
-    }),
-  ],
-  external: ['ws'],
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    plugins: [...plugins, terser()],
+    external: ['ws'],
+  },
+];
